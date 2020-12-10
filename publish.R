437b497949cf2b7dcc6a3e4c7f6e3d9f807d7b72
Aren Cambre <aren@arencambre.com>
2020-12-10 09:35:05 -0600

    routine updates

--- publish.R
+++ publish.R
@@ -1,7 +1,7 @@
 library(knitr)
 # if RWordPress isn't installed, uncomment and run the next two lines once
 # library(devtools)
-# devtools::install_github(c("duncantl/XMLRPC", "duncantl/RWordPress"))
+# devtools::install_github(c("josephguillaume/XMLRPC", "duncantl/RWordPress"))
 library(keyring)
 
 # you must use keyring::key_set() to set the below-referenced keys first
@@ -10,12 +10,16 @@ library(keyring)
 # Also, due to a limitation in how R's options work, the key name cannot
 # be set by providing a string. You'll need to replace `Aren Cambre` below
 # with your own username.
-options(WordpressLogin = c(`Aren Cambre` = key_get("WordPress publisher", "password")),
-        WordpressURL = paste0(key_get("WordPress publisher", "hostname"),
-                              'xmlrpc.php'))
+options(
+  WordpressLogin = c(`Aren Cambre` = key_get("WordPress publisher", "password")),
+  WordpressURL = paste0(key_get("WordPress publisher", "hostname"),
+                        'xmlrpc.php')
+)
 
-knit2wp(input = 'analysis.Rmd',
-        title = 'COVID-19 stats for Dallas and Texas',
-        publish = TRUE,
-        action = "editPost",
-        postid = 3275)
+knit2wp(
+  input = 'analysis.Rmd',
+  title = 'COVID-19 stats for Dallas and Texas',
+  publish = TRUE,
+  action = "editPost",
+  postid = 3275
+)
